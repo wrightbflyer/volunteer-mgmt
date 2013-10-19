68ad9f2cfa9421898053f54d53ddd23261f0924e
Andy Douglas <andy.douglas11@gmail.com>
2013-10-19 16:15:13 -0400

    membership types manager tweaks - change existing members on update and on delete allow user to specify replacement type

--- plugin/screens/manage_membership_types.php
+++ plugin/screens/manage_membership_types.php
@@ -4,8 +4,32 @@
 <?php 
 if (!empty($_POST))
 {
+	function update_members($newType)
+	{
+		global $wpdb;
+		$wpdb->update(
+			WBF_Membership::$member_table
+			,array(
+				'MemberType' => $newType
+			)
+			,array(
+				'MemberType' => $_POST['OriginalMemberType']
+			)
+			,array(
+				'%s'
+			)
+			,array(
+				'%s'
+			)
+		);
+	}
+
+	$result = -1;
 	if ($_POST['member_types_delete_mode'] == 'delete')
 	{
+		update_members($_POST['replacementMemberType']);
+		
+		// delete the membership type
 		$result = $wpdb->delete(
 			self::$member_type_table
 			,array(
@@ -36,6 +60,12 @@ if (!empty($_POST))
 				'%s'
 			)
 		);
+		
+		if ($_POST['OriginalMemberType'] != $_POST['MemberType'])
+		{
+			// the user is changing the MemberType of an existing type... update any members assigned to the type
+			update_members($_POST['MemberType']);
+		}
 	}
 	else
 	{
@@ -83,7 +113,9 @@ if (!empty($_POST))
     }
     else
     {
+		
         ?>
+		<h1><?php echo $result; ?></h1>
         <div class="error settings-error">
             <p>
                 <strong>Database Error - please check input</strong>
@@ -102,7 +134,9 @@ if (!empty($_POST))
 		overflow:auto;
 		max-height:400px;
     }
-	div.memberTypeEdit { background-color:rgba(255, 255, 0, .3); margin-top:20px; }
+	div.memberTypeEdit { background-color:rgba(255, 255, 0, .3); margin-top:20px; border:1px solid #000; }
+	
+	#divDelete { display:none; background-color:rgba(255, 0, 0, .5); margin-top:10px; }
 	
 	form label 
 	{ 
@@ -142,7 +176,7 @@ $member_types = self::get_member_types($wpdb); ?>
 
 <div class="memberTypeEdit memberTypes">
 	<?php
-	$id = $_REQUEST['id'];
+	$id = (empty($_REQUEST['id'])) ? null : $_REQUEST['id'];
 	if (!empty($id) && empty($_POST))
 	{ ?>
 	<b>Edit:</b>
@@ -169,9 +203,29 @@ $member_types = self::get_member_types($wpdb); ?>
 			<input type="hidden" id="member_types_delete_mode" name="member_types_delete_mode" />
 			<button type="button" id="member_types_delete">Delete</button>
 			<?php } ?>
-			<button type="submit" id="member_types_submit">Save</button>
+			<button type="submit" id="member_types_save">Save</button>
 		</div>
 		<div style="clear:both;"></div>
+		
+		<!-- Delete confirmation dynamically-displayed div -->
+		<div id="divDelete">
+			<div><b>Confirm Deletion</b></div>
+			<div>
+				Replace members assigned to this Membership Type with:
+				<select id="replacementMemberType" name="replacementMemberType">
+                    <option value="">Please choose a level</option>
+                    <?php foreach ( $member_types as $mt ) { 
+						if ($mt->MemberType == $edit_type->MemberType) continue; ?>
+                    <option value="<?php echo $mt->MemberType ?>"><?php echo $mt->MemberType ?></option>
+                    <?php } ?>
+                </select>
+			</div>
+			<div style="float:right;">
+				<button type="button" id="member_types_delete_cancel">Cancel Delete</button>
+				<button id="member_types_delete_confirm">Delete</button>
+			</div>
+			<div style="clear:both;"></div>
+		</div>
 	</form>
 	
 	<script>
@@ -185,18 +239,30 @@ $member_types = self::get_member_types($wpdb); ?>
                     jQuery(<?php echo json_encode("#$k");?>).val(<?php echo json_encode($v);?>);
                     <?php
                 }?>
-				jQuery('#member_types_delete').click(function(){
-					if (!confirm('Are you sure you want to delete this Membership Type?')) {
-						return;
-					}
-					jQuery('#member_types_delete_mode').val('delete');
-					jQuery('#member_types_submit').trigger('click');
-				});
 				
+				jQuery('#member_types_delete').click(deleteClick);
+				jQuery('#member_types_delete_cancel').click(cancelDelete);
+				jQuery('#member_types_delete_confirm').click(deleteConfirm);
 				jQuery('#MemberType').focus();
 				<?php
             }
             ?>
         });
+		
+		function deleteClick() {
+			jQuery(this).hide();
+			jQuery('#member_types_save').hide();
+			jQuery('#divDelete').show();
+		}
+		
+		function deleteConfirm() {
+			jQuery('#member_types_delete_mode').val('delete');
+			jQuery('#member_types_save').trigger('click');
+		}
+		
+		function cancelDelete() {
+			jQuery('#member_types_delete, #member_types_save').show();
+			jQuery('#divDelete').hide();
+		}
     </script>
 </div>
