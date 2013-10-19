61a6bc5c4945bcb3a98683f12906329e02ce64f9
Ryan Cromwell <ryan@echelontouch.com>
2013-10-19 18:18:11 -0400

    removed Membership list from snail and renewals

--- plugin/screens/renewals.php
+++ plugin/screens/renewals.php
@@ -1,5 +1,5 @@
     <h1>Membership Manager</h1>
-    <h2>Membership Listing - Renewals</h2>
+    <h2>Renewals</h2>
     <?php
     $members = self::get_member_renewal_list($wpdb);
     self::partial('member_listing',$members);
--- plugin/screens/snailmail.php
+++ plugin/screens/snailmail.php
@@ -1,5 +1,5 @@
 <h1>Membership Manager</h1>
-<h2>Membership Listing - Snail Mail</h2>
+<h2>Snail Mail</h2>
 <?php 
 $members = self::get_member_snailmail_list($wpdb);
 self::partial('member_listing',$members);
