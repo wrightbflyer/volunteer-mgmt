f5ef57beeebc38f582dcc7f36060984ca11f2e1d
Ryan Cromwell <ryan@echelontouch.com>
2013-10-19 13:12:50 -0400

    removed edit single from lists

--- plugin/screens/renewals.php
+++ plugin/screens/renewals.php
@@ -1,22 +1,4 @@
-<h1>Membership Manager</h1>
-<?php
-if (!empty($_REQUEST['id']))
-{
-    ?>
-    <h2>View / Edit Member</h2>
-    <?php
-    $sql = "SELECT * FROM " . self::$member_table . " WHERE ID=" . self::db_number($_REQUEST['id']);
-    $member = $wpdb->get_row($sql);
-    $member->RenewalDate = date("m/d/Y",strtotime($member->RenewalDate));
-    $member->MemberSince = date("m/d/Y",strtotime($member->MemberSince));
-    self::partial('member_form',$member);
-    ?>
-    <script>jQuery(document).ready(function($) { jQuery('#member_form_submit').html('Update Member'); });</script>
-    <?php
-}
-else
-{
-    ?>
+    <h1>Membership Manager</h1>
     <h2>Membership Listing - Renewals</h2>
     <?php
     // Calculate dates for start and end of this month
@@ -26,5 +8,3 @@ else
     $where = "RenewalDate < '$endDate'";
     $members = self::get_members($wpdb, $where);
     self::partial('member_listing',$members);
-}
-
--- plugin/screens/snailmail.php
+++ plugin/screens/snailmail.php
@@ -1,27 +1,7 @@
 <h1>Membership Manager</h1>
-<?php
-if (!empty($_REQUEST['id']))
-{
-    ?>
-    <h2>View / Edit Member</h2>
-    <?php
-    $sql = "SELECT * FROM " . self::$member_table . " WHERE ID=" . self::db_number($_REQUEST['id']);
-    $member = $wpdb->get_row($sql);
-    $member->RenewalDate = date("m/d/Y",strtotime($member->RenewalDate));
-    $member->MemberSince = date("m/d/Y",strtotime($member->MemberSince));
-    self::partial('member_form',$member);
-    ?>
-    <script>jQuery(document).ready(function($) { jQuery('#member_form_submit').html('Update Member'); });</script>
-    <?php
-}
-else
-{
-    ?>
-    <h2>Membership Listing - Snail Mail</h2>
-    <?php
-    
-    $members = self::get_members($wpdb, "Email <= ''" );
-    
-    self::partial('member_listing',$members);
-}
+<h2>Membership Listing - Snail Mail</h2>
+<?php 
+$members = self::get_members($wpdb, "Email <= ''" );
 
+self::partial('member_listing',$members);
+?>
